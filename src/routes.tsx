53d263160fc19a0f6de261a2a9a335821833d2e1
moch akbar maulana <moc.akbar119@gmail.com>
2023-03-21 20:01:21 +0700

    back to using main dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -13,13 +13,13 @@ import RTL from 'views/admin/rtl';
 import SignInCentered from 'views/auth/signIn';
 
 const routes = [
-	// {
-	// 	name: 'Main Dashboard',
-	// 	layout: '/admin',
-	// 	path: '/default',
-	// 	icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-	// 	component: MainDashboard
-	// },
+	{
+		name: 'Main Dashboard',
+		layout: '/admin',
+		path: '/default',
+		icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+		component: MainDashboard
+	},
 	// {
 	// 	name: 'NFT Marketplace',
 	// 	layout: '/admin',
