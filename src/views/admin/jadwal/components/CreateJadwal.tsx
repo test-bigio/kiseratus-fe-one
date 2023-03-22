dd7c497fb758b19a6e36870bd887669718aa6431
romzy <romziqutbi@gmail.com>
2023-03-22 14:10:01 +0700

    jadwal crud operation

--- src/views/admin/jadwal/components/CreateJadwal.tsx
+++ src/views/admin/jadwal/components/CreateJadwal.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Flex,
   FormControl,
   FormHelperText,
   FormLabel,
@@ -10,36 +11,118 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Textarea,
   useDisclosure,
 } from "@chakra-ui/react";
+import moment from "moment";
+import { useEffect, useState } from "react";
 
-export const CreateJadwal = ({
-  isOpen,
-  onClose,
-}: {
+interface props {
   isOpen: boolean;
   onClose: any;
-}) => {
+  events: Array<any>;
+  setRefresh: any;
+  eventInfo?: any;
+}
+
+export const CreateJadwal = (props: props) => {
+  const [judul, setJudul] = useState("");
+  const [deskripsi, setDeskripsi] = useState("");
+  const [tanggal, setTanggal] = useState(moment().format("YYYY-MM-DD"));
+
+  useEffect(() => {
+    if (props.isOpen) {
+      setJudul("");
+      setDeskripsi("");
+      setTanggal(moment().format("YYYY-MM-DD"));
+      if (props.eventInfo) {
+        // console.log(props.eventInfo.startStr);
+        setJudul(props.eventInfo.title);
+        setDeskripsi(props.eventInfo.extendedProps.description);
+        setTanggal(props.eventInfo.startStr);
+      }
+    }
+  }, [props.isOpen]);
+
+  const handleDelete = () => {
+    // e.preventDefault();
+    // console.log("ini delete");
+    let eventIndex = props.events.findIndex(
+      (obj) => obj.id == props.eventInfo.id
+    );
+    props.events.splice(eventIndex, 1);
+    localStorage.setItem("jadwal40", JSON.stringify(props.events));
+    props.onClose();
+    props.setRefresh(Date.now());
+  };
+  const handleSubmit = () => {
+    let body = { title: judul, description: deskripsi, date: tanggal };
+    if (props.eventInfo) {
+      console.log(props.eventInfo.id);
+      let eventIndex = props.events.findIndex(
+        (obj) => obj.id == props.eventInfo.id
+      );
+      let eventObj = props.events[eventIndex];
+      props.events[eventIndex] = { ...eventObj, ...body };
+      //   console.log(eventObj);
+    } else {
+      props.events.push({
+        id: Date.now(),
+        ...body,
+      });
+    }
+    localStorage.setItem("jadwal40", JSON.stringify(props.events));
+    props.onClose();
+    props.setRefresh(Date.now());
+  };
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={props.isOpen} onClose={props.onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Tambah Jadwal</ModalHeader>
           <ModalBody>
             <FormControl>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-              <FormHelperText>We'll never share your email.</FormHelperText>
+              <FormLabel>Judul</FormLabel>
+              <Input
+                type="text"
+                isRequired={true}
+                value={judul}
+                onChange={(e) => setJudul(e.target.value)}
+                // isReadOnly={true}
+              />
+              <FormLabel>Deskripsi</FormLabel>
+              {/* <Input type="text" isRequired={true} /> */}
+              <Textarea
+                isRequired={true}
+                defaultValue={deskripsi}
+                onChange={(e) => setDeskripsi(e.target.value)}
+              >
+                {/* {deskripsi} */}
+              </Textarea>
+              <FormLabel>Tanggal</FormLabel>
+              <Input
+                type="date"
+                isRequired={true}
+                value={tanggal}
+                onChange={(e) => setTanggal(e.target.value)}
+              />
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button variant="ghost" onClick={onClose}>
-              Batal
-            </Button>
-            <Button colorScheme="blue" mr={3} onClick={(e) => alert("click")}>
-              Simpan
-            </Button>
+            <Flex gap={"8px"}>
+              <Button variant="ghost" onClick={props.onClose}>
+                Batal
+              </Button>
+              {props.eventInfo && (
+                <Button colorScheme={"red"} onClick={handleDelete}>
+                  Hapus
+                </Button>
+              )}
+              <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
+                Simpan
+              </Button>
+            </Flex>
           </ModalFooter>
         </ModalContent>
       </Modal>
