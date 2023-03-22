b07b1418b6402b68c665aa1358919294b6d1144a
shaddamalbz <shaddam.albz@gmail.com>
2023-03-22 09:53:00 +0700

    add function to do calculation

--- src/views/admin/formula/components/CreateFormula.tsx
+++ src/views/admin/formula/components/CreateFormula.tsx
@@ -1,8 +1,10 @@
-import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
 import { showModal } from '../../../../stores/formula/formulaSlice'
+import { useAppDispatch, useAppSelector } from 'stores/hooks'
+import { RootState } from '../../../../stores/'
 
 // custom components
-import Card from 'components/card/Card';
+import Card from 'components/card/Card'
 
 // Chakra imports
 import {
@@ -12,13 +14,65 @@ import {
   FormLabel,
   useColorModeValue,
   Input
-} from "@chakra-ui/react";
+} from "@chakra-ui/react"
 
 
 const CreateFormula = () => {
-  const dispatch = useDispatch()
-
+  const calculation = useAppSelector((state:RootState) => state.formula.calculation)
+  const dispatch = useAppDispatch()
   const textColor = useColorModeValue("navy.700", "white");
+
+  const [result, setResult] = useState<number>(0)
+
+  const doCalculation = (calculation: string) => {
+      let arr = calculation.split('')
+      let num = ''
+  
+      for(let i = 0; i < calculation.length; i++) {
+        let char = calculation.charAt(i)
+        switch (char) {
+          case '+':
+          case 'x':
+          case '-':
+          case '/':
+            arr.push(num)
+            arr.push(char)
+            num = ''
+            break
+          default:
+            num += char
+            break
+        }
+      }
+      arr.push(num)
+  
+      let hasil = parseInt(arr[0])
+      for(let i = 1; i < arr.length; i += 2) {
+        switch (arr[i]) {
+          case '+':
+            hasil += parseInt(arr[i+1])
+            break
+          case 'x':
+            hasil *= parseInt(arr[i+1])
+            break
+          case '-':
+            hasil -= parseInt(arr[i+1])
+            break
+          case '/':
+            hasil /= parseInt(arr[i+1])
+            break
+        }
+      }
+      if(!hasil){
+        return 0
+      }
+      return hasil
+    }
+
+  useEffect(() => {
+    setResult(doCalculation(calculation))
+  }, [calculation])
+  
 
   return (
     <Card p='20px'>
@@ -44,6 +98,7 @@ const CreateFormula = () => {
             fontWeight='500'
             size='lg'
             disabled
+            value={result}
           />
           <Button
             fontSize='sm'
